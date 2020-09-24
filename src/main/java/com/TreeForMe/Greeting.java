84d2fd28a6d4ae3bb66683d84c7c15b979d44172
jack-delano-osu <54514875+jack-delano-osu@users.noreply.github.com>
2020-09-24 01:32:28 -0400

    keyword search for document title

--- src/main/java/com/TreeForMe/Greeting.java
+++ src/main/java/com/TreeForMe/Greeting.java
@@ -1,5 +1,10 @@
 package com.TreeForMe;
 
+import com.TreeForMe.Shared.DiscoveryService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Greeting {
 
     private final long id;
@@ -7,7 +12,11 @@ public class Greeting {
 
     public Greeting(long id, String content) {
         this.id = id;
-        this.content = content;
+        DiscoveryService disc = DiscoveryService.getInstance();
+        List<String> keywords = new ArrayList<>();
+        keywords.add("humidity");
+        keywords.add("aloe");
+        this.content = disc.getDocTitleFromKeywordSearch(keywords);
     }
 
     public long getId() {
